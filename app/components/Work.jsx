20e8796249798d9d9f0a52a2edb07202851acec4
zerim2 <jacob@pattersons.net>
2023-11-01 11:13:26 -0400

    Removed backslashes
    Backslashes do not compile and are considered comments

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -9,7 +9,7 @@ const Work = () => {
 			<div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
 				<div className='pb-8'>
 					<p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>Work</p>
-					<p className='py-6'>// Check out some of my recent projects</p>
+					<p className='py-6'> Check out some of my recent projects</p>
 				</div>
 				{/* Container */}
 				<div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
