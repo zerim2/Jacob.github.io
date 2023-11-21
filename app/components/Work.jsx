ce112e666d3dd89c72baaf9a5b9dd3e7e8def68a
zerim2 <jacob@pattersons.net>
2023-11-21 13:42:03 -0500

    Update Work.jsx

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -89,19 +89,19 @@ const Work = () => {
 					<div 
 					style={{backgroundImage: `url($(realEstate)`}}
 					 className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
-						
+
 						{/* Hover Effects*/}
 						<div className='opacity-0 group-hover:opacity-100 transition-all duration-700'>
 							<span className='text-2xl font-bold text-white tracking-wider'>
-							Scheduling App
+							Flight Travel Website
 							</span>
 
 							<div className='pt-8 text-center'>
-								<a href="/">
+								<a href="flightbooker-app.vercel.app">
 									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
 
 								</a>
-								<a href="/">
+								<a href="flightbooker-app.vercel.app">
 									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
 
 								</a>
