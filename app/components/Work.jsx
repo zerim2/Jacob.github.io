9baf1787d36e1bcdafbf51281df4d2d40e1558c8
zerim2 <jacob@pattersons.net>
2023-11-21 14:34:53 -0500

    Update Work.jsx

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -49,11 +49,11 @@ const Work = () => {
 							</span>
 
 							<div className='pt-8 text-center'>
-								<a href="/">
+								<a href="https://ecommerce-shop-starter-main-one.vercel.app">
 									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
 
 								</a>
-								<a href="/">
+								<a href="https://ecommerce-shop-starter-main-one.vercel.app">
 									<button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
 
 								</a>
